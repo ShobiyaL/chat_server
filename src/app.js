e27036b99f0809dba200bb6a7ac6baadfc198388
ShobiyaL <sofy7468612@gmail.com>
2023-04-23 16:52:45 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,14 @@ const publicRoute = require('./routes/public')
 const privateRoute = require('./routes/private')
 const authMiddleware = require('./middlewares/authMiddleware')
 
-app.use(cors())
+let corsOptions = {
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    exposedHeaders: ['x-auth-token']
+  };
+
+app.use(cors(corsOptions))
 app.use(express.json())
 
 app.use('/api/public',publicRoute)
